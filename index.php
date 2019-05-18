2a34e40e106278763f506fc41246fae1702de109
Tibinsunny <33937416+Tibinsunny@users.noreply.github.com>
2019-05-18 05:23:29 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<html><head><title>Index</title></head>
+    <body>
+        <form method=POST action=scrapper.php>
+            <input type=text name=url>
+            <input type=submit>
+            </form></body>
+</html>
