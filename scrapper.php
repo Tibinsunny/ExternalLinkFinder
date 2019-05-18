6e4eafa9ace9f4af4578a49210cc99ed5b23bfdb
Tibinsunny <33937416+Tibinsunny@users.noreply.github.com>
2019-05-18 05:24:37 -0700

    Update scrapper.php

--- scrapper.php
+++ scrapper.php
@@ -7,8 +7,7 @@ $dom = new DOMDocument();
 
 $xpath = new DOMXPath( $dom );
 $hrefs = $xpath->evaluate( "/html/body//a" );
-for( $i = 0; $i < 10; $i ++ ){
-//for( $i = 0; $i < $hrefs->length; $i ++ ) {
+for( $i = 0; $i < $hrefs->length; $i ++ ) {
 	$href = $hrefs->item( $i );
     $url = $href->getAttribute( 'href' );echo "<br>";
     
